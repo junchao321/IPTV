cff826ba7cb2d489b2c60cc52c80f9ec43103579
chao tang <Alantang_58@hotmail.com>
2025-04-16 14:41:05 +0800

    Update fetch.py

--- core/fetch.py
+++ core/fetch.py
@@ -1,25 +1,38 @@
-from utils.network import AsyncFetcher
-from utils.parser import ChannelParser
-import utils.file_ops as file_ops
+from utils import FileOps, AsyncFetcher, ChannelParser
+import asyncio
 
 class DataFetcher:
-    """数据获取核心类"""
-    def __init__(self):
+    """数据获取与解析核心类"""
+    def __init__(self, config: configparser.ConfigParser):
+        self.config = config
+        self.file_ops = FileOps()
         self.fetcher = AsyncFetcher()
         self.parser = ChannelParser()
     
-    async def fetch_subscribe_sources(self):
-        """获取自定义订阅源"""
-        urls = file_ops.read_lines('config/subscribe.txt')
-        contents = await self.fetcher.batch_fetch(urls)
-        return [self.parser.parse_m3u(content) for content in contents if content]
+    async def get_subscribe_channels(self) -> List[Dict]:
+        """获取并解析订阅源频道"""
+        subscribe_urls = self.file_ops.read_text_file(
+            self.config['PATHS']['subscribe_file']
+        )
+        contents = await self.fetcher.batch_fetch(subscribe_urls)
+        all_channels = []
+        for content in contents:
+            if content:
+                all_channels.extend(self.parser.parse_m3u_channels(content))
+        return all_channels
     
-    async def fetch_epg_sources(self):
-        """获取EPG源"""
-        urls = file_ops.read_lines('config/epg_sources.txt')
-        contents = await self.fetcher.batch_fetch(urls)
-        return {url: self.parser.parse_epg(content) for url, content in zip(urls, contents) if content}
+    async def get_epg_data(self) -> Dict[str, str]:
+        """获取并解析EPG数据"""
+        epg_urls = self.file_ops.read_text_file(
+            self.config['PATHS']['epg_source_file']
+        )
+        contents = await self.fetcher.batch_fetch(epg_urls)
+        epg_data = {}
+        for content in contents:
+            if content:
+                epg_data.update(self.parser.parse_epg_data(content))
+        return epg_data
     
-    def close(self):
+    async def close(self):
         """释放资源"""
-        self.fetcher.close()
+        await self.fetcher.close()
